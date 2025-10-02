4cc477757d0a252b8c4c37559b9fc5403eeee40d
mayedalfalasi <majed.oalfalasi@gmail.com>
2025-10-02 14:58:18 +0400

    Delete api directory

--- api/ping.js
+++
@@ -1,5 +0,0 @@
-// api/ping.js
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json({ ok: true, route: "ping", time: new Date().toISOString() });
-}
