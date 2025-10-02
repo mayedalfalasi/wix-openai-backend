c781c1a489b01396ff9dcf686d6584c2308d700b
mayedalfalasi <majed.oalfalasi@gmail.com>
2025-10-02 15:12:07 +0400

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,5 @@
-// api/ping.js
-export default function handler(req, res) {
+// api/ping.js  (CommonJS version)
+module.exports = (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json({ ok: true, route: "ping", time: new Date().toISOString() });
-}
+};
