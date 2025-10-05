f374e6619c14b4a8fab472f7e78d9f05c51df437
mayedalfalasi <majed.oalfalasi@gmail.com>
2025-10-05 13:05:08 +0400

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,12 +1,11 @@
-// api/ping.js
-function setCors(res) {
+function setCORS(res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 }
 
 export default function handler(req, res) {
-  setCors(res);
+  setCORS(res);
   if (req.method === "OPTIONS") return res.status(204).end();
   res.status(200).json({ ok: true, time: new Date().toISOString() });
 }
